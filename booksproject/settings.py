a900e28d974587c20bf3a29eaa0a7b2fbc759870
Josh Fitzgerald <x00188120@mutudublin.ie>
2022-11-10 09:42:41 +0000

    part 4

--- booksproject/settings.py
+++ booksproject/settings.py
@@ -144,3 +144,6 @@ LOGOUT_REDIRECT_URL = 'home'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+MEDIA_UL = '/media'
+MEDIA_ROOT = str(BASE_DIR.joinpath('media'))
